f9f136b036bcd98bd22044286d05391ff40ae801
1MishGun1 <mikhail.kudrov@gmail.com>
2023-08-29 14:57:09 +0300

    Update (edit slider and scroll nav)

--- scripts/script.js
+++ scripts/script.js
@@ -14,14 +14,28 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
 });
 
 const btnPayProduct = document.querySelectorAll(".product-button");
-const btnOpenCatalog = document.querySelectorAll(".btn-catalog");
+const btnOpenCatalog = document.querySelector(".btn-catalog");
 const btnPost = document.querySelectorAll(".news-item");
-const btnOpenNews = document.querySelectorAll(".btn-news");
+const btnOpenNews = document.querySelector(".btn-news");
 
 const openPageProduct = function () {
   window.open("product.html");
 };
-
 for (let i = 0; i < btnPayProduct.length; i++) {
   btnPayProduct[i].addEventListener("click", openPageProduct);
 }
+
+btnOpenCatalog.addEventListener("click", function () {
+  window.open("shop.html");
+});
+
+const openCatalog = function () {
+  window.open("post.html");
+};
+for (let i = 0; i < btnPost.length; i++) {
+  btnPost[i].addEventListener("click", openCatalog);
+}
+
+btnOpenNews.addEventListener("click", function () {
+  window.open("shop.html");
+});
