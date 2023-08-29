6e74764c1947efff5d0fdcc7fc730a0048b559b3
1MishGun1 <mikhail.kudrov@gmail.com>
2023-08-29 15:08:13 +0300

    Update catalog scripts

---
+++ scripts/catalog.js
@@ -0,0 +1,14 @@
+const btnlogo = document.querySelector(".logo");
+
+const btnProductItem = document.querySelectorAll(".catalog-item");
+
+btnlogo.addEventListener("click", function () {
+  window.open("index.html");
+});
+
+const openPageProduct = function () {
+  window.open("product.html");
+};
+for (let i = 0; i < btnProductItem.length; i++) {
+  btnProductItem[i].addEventListener("click", openPageProduct);
+}
