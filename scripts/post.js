5baab7bfbbaaf7fb544f861c53f0960ca8b06c7b
1MishGun1 <mikhail.kudrov@gmail.com>
2023-08-29 18:49:17 +0300

    Update post scripts

---
+++ scripts/post.js
@@ -0,0 +1,10 @@
+const btnlogo = document.querySelector(".logo");
+const btnOpenShop = document.querySelector(".btn-shop");
+
+btnOpenShop.addEventListener("click", function () {
+  window.open("basket.html");
+});
+
+btnlogo.addEventListener("click", function () {
+  window.open("index.html");
+});
