1c9e0c5578f6006e53a0b0a5121eb54131a316ca
1MishGun1 <mikhail.kudrov@gmail.com>
2023-08-30 23:06:17 +0300

    Update scripts

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,7 @@
 //TODO Scroll
-document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth",
-    });
-
-    document.querySelectorAll("a").forEach((el) => {
-      el.classList.remove("scroll-active");
-    });
-    this.classList.add("scroll-active");
-  });
-});
+//
 
 //TODO Slider_Catalog
-// let swiper = new Swiper(".mySwiper", {
-//   slidesPerView: 3,
-//   spaceBetween: 30,
-//   slidesPerGroup: 3,
-//   loop: true,
-//   loopFillGroupWithBlank: true,
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-// });
 
 //TODO Buttons
 const btnProduct = document.querySelectorAll(".products-item");
