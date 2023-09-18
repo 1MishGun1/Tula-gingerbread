33898da910a8ad32eba4291478cdd4c655479a7d
1MishGun1 <mikhail.kudrov@gmail.com>
2023-09-18 23:45:12 +0300

    Update index scripts (slider)

--- scripts/script.js
+++ scripts/script.js
@@ -12,49 +12,6 @@ window.onscroll = function () {
 };
 
 //TODO Slider_Catalog
-// let position = 0;
-// const slidesShow = 3;
-// const slidesScroll = 1;
-// const container = document.querySelector(".slider-container");
-// const track = document.querySelector(".slider-track");
-// const items = document.querySelectorAll(".slider-item");
-// const btnPrev = document.querySelector(".btn-prev");
-// const btnNext = document.querySelector(".btn-next");
-// const countItem = items.length;
-// const itemWidth = container.clientWidth / slidesShow;
-// const movePosition = slidesScroll * itemWidth;
-
-// items.forEach((item) => {
-//   item.style.minWidth = `${itemWidth}px`;
-// });
-
-// btnNext.addEventListener("click", () => {
-//   const itemsLeft =
-//     countItem - (Math.abs(position) + slidesShow * itemWidth) / itemWidth;
-
-//   position -= itemsLeft >= slidesScroll ? movePosition : itemsLeft * itemWidth;
-//   setPosition();
-//   checkBtns();
-// });
-
-// btnPrev.addEventListener("click", () => {
-//   const itemsLeft = Math.abs(position) / itemWidth;
-
-//   position += itemsLeft >= slidesScroll ? movePosition : itemsLeft * itemWidth;
-//   setPosition();
-//   checkBtns();
-// });
-
-// const setPosition = () => {
-//   track.style.transform = `translateX(${position}px)`;
-// };
-
-// const checkBtns = () => {
-//   btnPrev.disabled = position === 0;
-//   btnNext.disabled = position <= -(countItem - slidesShow) * itemWidth;
-// };
-// checkBtns();
-
 let swiper = new Swiper(".slider-container", {
   slidesPerView: 3,
   spaceBetween: 30,
