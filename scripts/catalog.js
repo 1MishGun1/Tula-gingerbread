67cd598fd0a197387be3d2bfa9b8594dbe8722db
1MishGun1 <mikhail.kudrov@gmail.com>
2023-09-19 09:18:00 +0300

    Update catalog script (delete open page)

--- scripts/catalog.js
+++ scripts/catalog.js
@@ -1,14 +1,4 @@
-const btnlogo = document.querySelector(".logo");
 const btnProductItem = document.querySelectorAll(".catalog-item");
-const btnOpenShop = document.querySelector(".btn-shop");
-
-btnOpenShop.addEventListener("click", function () {
-  window.open("basket.html");
-});
-
-btnlogo.addEventListener("click", function () {
-  window.open("index.html");
-});
 
 const openPageProduct = function () {
   window.open("product.html");
